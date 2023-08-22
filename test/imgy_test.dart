7f282ebf6380fa061fcd5928770d78a5a53b499d
Carlos Costa <carllos.nc@gmail.com>
2023-08-22 19:30:06 -0300

    new test case

--- test/imgy_test.dart
+++ test/imgy_test.dart
@@ -4,12 +4,12 @@ import 'package:imgy/imgy.dart';
 import 'package:network_image_mock/network_image_mock.dart';
 
 void main() {
-  testWidgets(
-    'Check Imgy component initial render and interaction',
-    (tester) async {
-      var imageAddress =
-          "https://images.unsplash.com/photo-1682685797229-b2930538da47?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80";
+  var imageAddress =
+      "https://images.unsplash.com/photo-1682685797229-b2930538da47?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80";
 
+  testWidgets(
+    'Check Imgy component initial render and behavior',
+    (tester) async {
       await mockNetworkImagesFor(() async {
         return tester.pumpWidget(
           MaterialApp(
@@ -36,11 +36,63 @@ void main() {
       //tap on image preview to open full screen
 
       await tester.tap(find.byKey(const Key('imgy_preview_container')));
+
       await tester.pumpAndSettle();
 
       //can't see because {enableFullScreen} is false
 
       expect(find.byKey(const Key('imgy_full_screen_container')), findsNothing);
+    },
+  );
+
+  testWidgets(
+    'Check full screen render and behavior',
+    (tester) async {
+      await mockNetworkImagesFor(() async {
+        return tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: SizedBox(
+                child: Imgy(
+                  src: imageAddress,
+                  fullSrc: imageAddress,
+                  enableFullScreen: true,
+                ),
+              ),
+            ),
+          ),
+        );
+      });
+
+      //tap on image preview to open full screen
+
+      await tester.tap(find.byKey(const Key('imgy_preview_container')));
+      await tester.pump();
+
+      //can see full screen because {enableFullScreen} is true
+
+      expect(
+          find.byKey(const Key('imgy_full_screen_container')), findsOneWidget);
+      expect(find.byKey(const Key('imgy_full_screen_image')), findsOneWidget);
+
+      //check header and buttons
+
+      expect(find.byKey(const Key('imgy_full_screen_header')), findsOneWidget);
+      expect(find.byKey(const Key('imgy_full_screen_close')), findsOneWidget);
+      expect(find.byKey(const Key('imgy_full_screen_share')), findsOneWidget);
+      expect(
+          find.byKey(const Key('imgy_full_screen_download')), findsOneWidget);
+      expect(find.byKey(const Key('imgy_full_screen_check')), findsNothing);
+
+      //tap on close button to close full screen
+
+      await tester.tap(find.byKey(const Key('imgy_full_screen_close')));
+      await tester.pump();
+
+      //can't see full screen because it's hidden
+
+      expect(find.byKey(const Key('imgy_full_screen_container')), findsNothing);
+      expect(find.byKey(const Key('imgy_full_screen_image')), findsNothing);
     },
   );
 }
