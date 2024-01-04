3983c76cb470f2d9d41ef7ae185b394c090588c2
Carlos Costa <carllos.nc@gmail.com>
2024-01-03 21:20:19 -0300

    Refactor ImgyFullScreen class in ImgyFullScreenWebImage

--- lib/imgy_full_screen.dart
+++ lib/imgy_full_screen.dart
@@ -32,53 +32,44 @@ class ImgyFullScreenWebImage extends StatelessWidget {
 }
 
 class ImgyFullScreen extends StatelessWidget {
+  final Imgy widget;
+
   const ImgyFullScreen({
     super.key,
     required this.widget,
   });
 
-  final Imgy widget;
-
   @override
   Widget build(BuildContext context) {
-    final GlobalKey globalKey = GlobalKey();
-
-    return Positioned.fill(
-      child: Stack(
-        key: const Key('imgy_full_screen_container'),
-        children: [
-          Center(
-            child: Container(
-              key: const Key('imgy_full_screen_image'),
-              constraints: const BoxConstraints(
-                maxWidth: 500,
-              ),
-              child: InteractiveViewer(
-                clipBehavior: Clip.none,
-                maxScale: 5.0,
-                minScale: 1.0,
-                child: GestureDetector(
-                  key: const Key('imgy_full_screen_gesture'),
-                  onTap: () {
-                    if (widget.tapImageToClose) {
-                      Navigator.of(context).pop();
-                    }
-                  },
-                  child: RepaintBoundary(
-                    key: globalKey,
-                    child: Container(
-                      color: Colors.transparent,
-                      child: checkFullSrc(widget.fullSrc)
-                          ? ImgyFullScreenWebImage(widget: widget)
-                          : Image.asset(widget.fullSrc, fit: BoxFit.cover),
-                    ),
-                  ),
+    return Stack(
+      key: const Key('imgy_full_screen_container'),
+      children: [
+        Center(
+          child: Container(
+            key: const Key('imgy_full_screen_image'),
+            constraints: const BoxConstraints(
+              maxWidth: 500,
+            ),
+            child: InteractiveViewer(
+              clipBehavior: Clip.none,
+              maxScale: 5.0,
+              minScale: 1.0,
+              child: GestureDetector(
+                key: const Key('imgy_full_screen_gesture'),
+                onTap: () {
+                  if (widget.tapImageToClose) {
+                    Navigator.of(context).pop();
+                  }
+                },
+                child: Container(
+                  color: Colors.transparent,
+                  child: checkFullSrc(widget.fullSrc) ? ImgyFullScreenWebImage(widget: widget) : Image.asset(widget.fullSrc, fit: BoxFit.cover),
                 ),
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
